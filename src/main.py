fb9b0fb758429ffc427b67ec04df1399e104300a
Yegor Zinovyev <egor.zinovjev2013@yandex.ru>
2022-02-05 20:34:00 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,8 +5,7 @@ import numpy as np
 
 def init():
     create_biffurcation_diagram()
-    # The results of the bifurcation diagram
-    # indicate the need to use the value 0.9 as the parameter p
+    # Based on pictures of the bifurcation diagram
     for p in np.arange(0.9, 0.92, 0.01):
         create_pool(p)
 
